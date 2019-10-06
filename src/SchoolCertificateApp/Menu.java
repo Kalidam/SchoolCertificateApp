43df0cb331e615d9f9972ec52f7859ddb6a487c5
Damian <kalidam@gmail.com>
2019-10-06 22:10:55 +0200

    Updated menu

--- src/SchoolCertificateApp/Menu.java
+++ src/SchoolCertificateApp/Menu.java
@@ -5,10 +5,36 @@
  */
 package SchoolCertificateApp;
 
+
 /**
  *
  * @author Dell
  */
 public class Menu {
-    
+
+    public void printMenu () {
+        System.out.println("=====================================");
+        System.out.println();
+        System.out.println("  APLIKACJA DO OBSŁUGI OCEN UCZNIÓW");
+        System.out.println();
+        System.out.println("=====================================");
+        System.out.println();
+        System.out.println("1. ");
+
+    }
+
+    public void chooseOptionFromMenu () {
+        int choosenOption = ScannerUtility.getIntegerFromUser("Wybierz odpowiednią opcję:");
+        switch (choosenOption){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+
+    }
 }
