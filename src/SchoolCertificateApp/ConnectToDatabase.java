b67db9def78f081e1afdcf46c2c6ad285abd7318
Damian <kalidam@gmail.com>
2019-10-07 19:59:49 +0200

    Created connection with local database

--- src/SchoolCertificateApp/ConnectToDatabase.java
+++ src/SchoolCertificateApp/ConnectToDatabase.java
@@ -5,10 +5,32 @@
  */
 package SchoolCertificateApp;
 
+import java.sql.*;
+
 /**
  *
  * @author Dell
  */
 public class ConnectToDatabase {
-    
+    public ConnectToDatabase{
+        String query;
+        try {
+            Class.forName("com.mysgl.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schoolcertificateappdatabase",
+                    "root", "");
+            Statement stmt = con.createStatement();
+            //query = metoda zwracajaca stringa
+            while (query != null) {
+                ResultSet rs = stmt.executeQuery(query);
+                //query = metoda zwracajaca stringa
+            }
+            con.close();
+        }
+        catch (SQLException e) {
+            System.err.println("Nie znaleziono bazy danych!");
+        }
+        catch (ClassNotFoundException e) {
+            System.err.println("Nie można połączyć z bazą danych!");;
+        }
+    }
 }
